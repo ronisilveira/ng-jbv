03e5574dd820d01544663a82ee100aaee6f9cab4
Roni Silveira <roni@Server.E4sy>
2019-04-11 12:31:02 -0300

    Fixies

--- src/jbverror-handler.ts
+++ src/jbverror-handler.ts
@@ -8,12 +8,9 @@ export class JBVErrorHandler implements ErrorHandler {
 
     handleError(response: any): void {
 
-        if (response.status == 400) {
-            if (response.has("error"))
-                this.jbvService.addErrors(response.error.errors);
-            else
-                this.jbvService.addErrors(response.json().errors);
-        } else
+        if (response.status == 400)
+            this.jbvService.addErrors(response.error.errors);
+        else
             console.error(response);
     }
 }
